7eab13758b939dbaec9a4fe2c6cd3a62c652361b
phoenix-31 <tom3.9.27.81.243@gmail.com>
2021-07-15 11:17:17 +1000

    idk

--- src/epilepsy.js
+++ src/epilepsy.js
@@ -2,5 +2,5 @@ function epilepsy() {
     document.body.style.backgroundColor = "rgb(" + Math.random() * 256 + ", " + Math.random() * 256 + ", " + Math.random() * 256 + ")"
     setInterval(() => {
         document.body.style.backgroundColor = "rgb(" + Math.random() * 256 + ", " + Math.random() * 256 + ", " + Math.random() * 256 + ")"
-    }, 1000);
+    }, 1);
 }
